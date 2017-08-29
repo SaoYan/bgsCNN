84125e9f21bef7c0d8f52da724c1f319137a0671
saoyan <yanyiqinwpu@gmail.com>
2017-08-29 14:51:27 -0600

    update

--- prepare_data.py
+++ prepare_data.py
@@ -48,31 +48,32 @@ def prepare_data(root_dir, height, width):
                 frame_filename = root_dir + "/" + dirname_l0 + "/input/" + num2filename(num, "in") + ".jpg"
                 bg_filename = root_dir + "/" + dirname_l0 + "/bg/" + num2filename(num, "bg") + ".jpg"
                 gt_filename = root_dir + "/" + dirname_l0 + "/groundtruth/" + num2filename(num, "gt") + ".png"
-                frame = cv2.imread(frame_filename)
-                frame[roi_img == 0] = 0
-                frame = cv2.resize(frame, (width, height), interpolation = cv2.INTER_CUBIC)
-                bg_model = cv2.imread(bg_filename)
-                bg_model[roi_img == 0] = 0
-                bg_model = cv2.resize(bg_model, (width, height), interpolation = cv2.INTER_CUBIC)
                 gt_mask = cv2.imread(gt_filename)
-                gt_mask[(gt_mask != 0) & (gt_mask != 255)] = 0
-                gt_mask = cv2.resize(gt_mask, (width, height))
-                gt_mask[gt_mask > 0] = 255
-
-                gt = gt_mask[:,:,0]
-                gt = np.expand_dims(gt, axis = 2)
-                data_cube = np.uint8(np.concatenate([frame, bg_model, gt], 2))
-                image_raw = data_cube.tostring()
-                feature={ 'image_raw': _bytes_feature(image_raw) }
-                example = tf.train.Example(features=tf.train.Features(feature=feature))
-                if dirname_l0 != "highway":
-                    train_writer.write(example.SerializeToString())
-                    total_num_train = total_num_train + 1
-                else:
-                    test_writer.write(example.SerializeToString())
-                    total_num_test = total_num_test + 1
-                print("add data for frame # " + str(num))
-                num = num + 1
+                flag = (gt_mask==0) | (gt_mask==255)
+                if flag.any()
+                    frame = cv2.imread(frame_filename)
+                    frame[roi_img == 0] = 0
+                    frame = cv2.resize(frame, (width, height), interpolation = cv2.INTER_CUBIC)
+                    bg_model = cv2.imread(bg_filename)
+                    bg_model[roi_img == 0] = 0
+                    bg_model = cv2.resize(bg_model, (width, height), interpolation = cv2.INTER_CUBIC)
+                    gt_mask[(gt_mask != 0) & (gt_mask != 255)] = 0
+                    gt_mask = cv2.resize(gt_mask, (width, height))
+                    gt_mask[gt_mask > 0] = 255
+                    gt = gt_mask[:,:,0]
+                    gt = np.expand_dims(gt, axis = 2)
+                    data_cube = np.uint8(np.concatenate([frame, bg_model, gt], 2))
+                    image_raw = data_cube.tostring()
+                    feature={ 'image_raw': _bytes_feature(image_raw) }
+                    example = tf.train.Example(features=tf.train.Features(feature=feature))
+                    if dirname_l0 != "highway":
+                        train_writer.write(example.SerializeToString())
+                        total_num_train = total_num_train + 1
+                    else:
+                        test_writer.write(example.SerializeToString())
+                        total_num_test = total_num_test + 1
+                    print("add data for frame # " + str(num))
+                    num = num + 1
             print ("finish dealing with " + dirname_l0 + "\n")
     print ("total # of training samples: " + str(total_num_train))
     print ("total # of test samples: " + str(total_num_test))
